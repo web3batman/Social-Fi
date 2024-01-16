3269da6bab1a121f66d36e0a08a70a49cbc4c980
web3batman <leobrandt0912@gmail.com>
2024-01-15 21:04:51 -0800

    fix router error

--- src/components/bottom_nav.tsx
+++ src/components/bottom_nav.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import styles from './sidebar.module.css'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const BottomNav = () => {
   const router = useRouter();
@@ -13,36 +14,36 @@ const BottomNav = () => {
   return (
     <div className='fixed bottom-0 w-full md:hidden z-20'>
       <div className='flex rounded-t-[15px] bg-white dark:bg-dark-header-bg p-2'>
-        <div className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/home' ? styles.active : 'text-grey-1'}`} onClick={() => router.push('/home')}>
+        <Link href={'/home'} className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/home' ? styles.active : 'text-grey-1'}`}>
           <div className='flex flex-col items-center gap-2'>
             <Image quality={100} src={currentPage != '/home' ? '/icons/home.svg' : '/icons/home_active.svg'} width={100} height={100} alt='Home' className='w-6 h-6' />
             <h3 className='font-semibold leading-[14px] text-base'>Home</h3>
           </div>
-        </div>
-        <div className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/search' ? styles.active : 'text-grey-1'}`} onClick={() => router.push('/search')}>
+        </Link>
+        <Link href={'/search'} className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/search' ? styles.active : 'text-grey-1'}`}>
           <div className='flex flex-col items-center gap-2'>
             <Image quality={100} src={currentPage != '/search' ? '/icons/side_search.svg' : '/icons/side_search_active.svg'} width={100} height={100} alt='Home' className='w-6 h-6' />
             <h3 className='font-semibold leading-[14px] text-base'>Search</h3>
           </div>
-        </div>
-        <div className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/keys' ? styles.active : 'text-grey-1'}`} onClick={() => router.push('/keys')}>
+        </Link>
+        <Link href={'/keys'} className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/keys' ? styles.active : 'text-grey-1'}`}>
           <div className='flex flex-col items-center gap-2'>
             <Image quality={100} src={currentPage != '/keys' ? '/icons/Keys.svg' : '/icons/Keys_active.svg'} width={100} height={100} alt='Home' className='w-6 h-6' />
             <h3 className='font-semibold leading-[14px] text-base max-w-full overflow-hidden'>Keys</h3>
           </div>
-        </div>
-        <div className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/inbox' ? styles.active : 'text-grey-1'}`} onClick={() => router.push('/inbox')}>
+        </Link>
+        <Link href={'/inbox'} className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/inbox' ? styles.active : 'text-grey-1'}`}>
           <div className='flex flex-col items-center gap-2'>
             <Image quality={100} src={currentPage != '/inbox' ? '/icons/side_inbox.svg' : '/icons/side_inbox_active.svg'} width={100} height={100} alt='Home' className='w-6 h-6' />
             <h3 className='font-semibold leading-[14px] text-base'>Inbox</h3>
           </div>
-        </div>
-        <div className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/wallet' ? styles.active : 'text-grey-1'}`} onClick={() => router.push('/wallet')}>
+        </Link>
+        <Link href={'/wallet'} className={`p-4 cursor-pointer hover:bg-gray-300 rounded-x-lg w-1/5 ${currentPage == '/wallet' ? styles.active : 'text-grey-1'}`}>
           <div className='flex flex-col items-center gap-2'>
             <Image quality={100} src={currentPage != '/wallet' ? '/icons/side_wallet.svg' : '/icons/side_wallet_active.svg'} width={100} height={100} alt='Home' className='w-6 h-6' />
             <h3 className='font-semibold leading-[14px] text-base'>Wallet</h3>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   )
